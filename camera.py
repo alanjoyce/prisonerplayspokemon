145b3a09c97e1b2230aea2ef963923a0d1765dd8
Alan Joyce <alan@thisisalan.com>
2014-08-17 01:29:15 -0700

    keyboard presses work

--- camera.py
+++ camera.py
@@ -1,11 +1,26 @@
 import cv2
 import numpy as np
+import os
+import time
 
 c = cv2.VideoCapture(0)
 
 prevPrevPieces = []
 prevPieces = []
 mov = []
+
+lastPress = time.time()
+lastButton = ""
+
+def pressButton(char):
+	global lastPress
+	global lastButton
+	
+	#Rate limit to 1 repeated button press per second
+	if (time.time() - lastPress >= 1) or char != lastButton:
+		cmd = "osascript -e 'tell application \"System Events\" to keystroke \"" + char + "\"'"
+		os.system(cmd)
+		lastPress = time.time()
 
 while(1):
 	img = c.read()[1]
@@ -48,27 +63,52 @@ while(1):
 			totalDiff = sum(sum(x) for x in result)
 			if totalDiff > 1000:
 				mov[i][j] = 255
-			elif mov[i][j] > 0:
-				mov[i][j] = max(mov[i][j] - 25, 0)
+			else:
+				mov[i][j] = 0
+	
+	#Row 1
+	if mov[0][0]:
+		pressButton("a")
+	cv2.rectangle(img, (0,0), (width/3,height/3), (mov[0][0],0,0), 5)
+	cv2.putText(img, "A", (width/6, height/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	if mov[0][1]:
+		pressButton("u")
+	cv2.rectangle(img, (width/3,0), (width*2/3,height/3), (mov[0][1],0,0), 5)
+	cv2.putText(img, "U", (width/2, height/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	if mov[0][2]:
+		pressButton("b")
+	cv2.rectangle(img, (width*2/3,0), (width,height/3), (mov[0][2],0,0), 5)
+	cv2.putText(img, "B", (width*5/6, height/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	
+	#Row 2
+	if mov[1][0]:
+		pressButton("l")
+	cv2.rectangle(img, (0,height/3), (width/3,height*2/3), (mov[1][0],0,0), 5)
+	cv2.putText(img, "L", (width/6, height/2), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	cv2.rectangle(img, (width/3,height/3), (width*2/3,height*2/3), (mov[1][1],0,0), 5)
+	cv2.putText(img, "", (width/2, height/2), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	if mov[1][2]:
+		pressButton("r")
+	cv2.rectangle(img, (width*2/3,height/3), (width,height*2/3), (mov[1][2],0,0), 5)
+	cv2.putText(img, "R", (width*5/6, height/2), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	
+	#Row 3
+	if mov[2][0]:
+		pressButton("e")
+	cv2.rectangle(img, (0,height*2/3), (width/3,height), (mov[2][0],0,0), 5)
+	cv2.putText(img, "SEL", (width/6, height*5/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	if mov[2][1]:
+		pressButton("d")
+	cv2.rectangle(img, (width/3,height*2/3), (width*2/3,height), (mov[2][1],0,0), 5)
+	cv2.putText(img, "D", (width/2, height*5/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
+	if mov[2][2]:
+		pressButton("s")
+	cv2.rectangle(img, (width*2/3,height*2/3), (width,height), (mov[2][2],0,0), 5)
+	cv2.putText(img, "ST", (width*5/6, height*5/6), cv2.FONT_HERSHEY_PLAIN, 3, 255, 5)
 	
 	#Update previousPieces
 	prevPrevPieces = prevPieces
 	prevPieces = pieces
-	
-	#Row 1
-	cv2.rectangle(img, (0,0), (width/3,height/3), (mov[0][0],0,0), 5)
-	cv2.rectangle(img, (width/3,0), (width*2/3,height/3), (mov[0][1],0,0), 5)
-	cv2.rectangle(img, (width*2/3,0), (width,height/3), (mov[0][2],0,0), 5)
-	
-	#Row 2
-	cv2.rectangle(img, (0,height/3), (width/3,height*2/3), (mov[1][0],0,0), 5)
-	cv2.rectangle(img, (width/3,height/3), (width*2/3,height*2/3), (mov[1][1],0,0), 5)
-	cv2.rectangle(img, (width*2/3,height/3), (width,height*2/3), (mov[1][2],0,0), 5)
-	
-	#Row 3
-	cv2.rectangle(img, (0,height*2/3), (width/3,height), (mov[2][0],0,0), 5)
-	cv2.rectangle(img, (width/3,height*2/3), (width*2/3,height), (mov[2][1],0,0), 5)
-	cv2.rectangle(img, (width*2/3,height*2/3), (width,height), (mov[2][2],0,0), 5)
 	
 	cv2.imshow('e2', img)
 	if cv2.waitKey(5) == 27:
